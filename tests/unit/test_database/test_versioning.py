183e643c5799a088fb4a387ae42fc0bdc46e67a2
Michal Kononenko <michalkononenko@gmail.com>
2015-12-17 13:37:01 -0500

    Fixed coverage error, concatenated builtin string resolution to 1 line

--- tests/unit/test_database/test_versioning.py
+++ tests/unit/test_database/test_versioning.py
@@ -12,10 +12,7 @@ __author__ = 'Michal Kononenko'
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-if sys.version_info < (3,):
-    builtin_string = '__builtin__'
-else:
-    builtin_string = 'builtins'
+builtin_string = '__builtin__' if sys.version.info < (3,) else 'builtins'
 
 
 class MockMigrateAPI(mock.MagicMock):
