4114852401665efa7bec96cd55f662433232926f
Michal Kononenko <michalkononenko@gmail.com>
2016-01-02 20:30:39 -0500

    Refactored search query parsing unit tests

--- tests/unit/test_views/test_users.py
+++ tests/unit/test_views/test_users.py
@@ -61,107 +61,70 @@ class TestUserContainer(TestView):
 
 
 class TestParseSearchQueryParams(TestUserContainer):
-
     def setUp(self):
-        TestUserContainer.setUp(self)
-        self.request_method = self.client.get
-        self.template_url = 'api/v1/users'
+        self.url = '/api/v1/users'
 
-        user = User(self.username, self.password, self.email)
 
-        with users.database_session as session:
-            session.add(user)
-
-        self.top_level_json_key = 'users'
-        self.api_charset = 'utf-8'
-
-    def tearDown(self):
-
-        with users.database_session as session:
-            user = session.query(User).filter_by(
-                username=self.username
-            ).first()
-
-            session.delete(user)
-
-    def test_contains_match_user(self):
-        contains_string = self.username[1:-1]
-        request_url = '%s?contains=%s' % (self.template_url, contains_string)
-
-        json_dict = self._get_request(request_url, 1)
-
-        self.assertTrue(json_dict[self.top_level_json_key])
-
-    def test_contains_user_not_found(self):
-        contains_string = 'abc'
-        self.assertNotIn(contains_string, self.username)
-
-        request_url = '%s?contains=%s' % (self.template_url, contains_string)
-
-        json_dict = self._get_request(request_url, 0)
-
-        self.assertFalse(json_dict[self.top_level_json_key])
-
-    def test_begins_with_user_found(self):
-        begins_with_string = self.username[0:2]
-        request_url = '%s?starts_with=%s' % (
-            self.template_url, begins_with_string
+class TestParseSearchQueryParamsContains(TestParseSearchQueryParams):
+    def setUp(self):
+        TestParseSearchQueryParams.setUp(self)
+        self.contains_string = 'string'
+        self.context = app.test_request_context(
+            '%s?contains=%s' % (self.url, self.contains_string)
         )
 
-        json_dict = self._get_request(request_url, 1)
+    def test_contains_string_is_not_none(self):
+        with self.context:
+            search_string = users.UserContainer.parse_search_query_params(
+                    users.request)
 
-        self.assertTrue(json_dict[self.top_level_json_key])
+            self.assertEqual('%%%s%%' % self.contains_string, search_string)
 
-    def test_begins_with_user_not_found(self):
-        begins_with_string = 'y'
 
-        self.assertNotIn(begins_with_string, self.username)
-
-        request_url = '%s?starts_with=%s' % (
-            self.template_url, begins_with_string
+class TestParseSearchQueryParamsStartsWith(TestParseSearchQueryParams):
+    def setUp(self):
+        TestParseSearchQueryParams.setUp(self)
+        self.contains_string = 'string'
+        self.context = app.test_request_context(
+            '%s?starts_with=%s' % (self.url, self.contains_string)
         )
 
-        json_dict = self._get_request(request_url, 0)
+    def test_starts_with(self):
+        with self.context:
+            search_string = users.UserContainer.parse_search_query_params(
+                users.request
+            )
+            self.assertEqual('%s%%' % self.contains_string, search_string)
 
-        self.assertFalse(json_dict[self.top_level_json_key])
 
-    def test_ends_with_user_found(self):
-        ends_with_string = self.username[-1:-3]
-
-        request_url = '%s?ends_with=%s' % (
-            self.template_url, ends_with_string
+class TestParseSearchQueryParamsEndsWith(TestParseSearchQueryParams):
+    def setUp(self):
+        TestParseSearchQueryParams.setUp(self)
+        self.contains_string = 'string'
+        self.context = app.test_request_context(
+            '%s?ends_with=%s' % (self.url, self.contains_string)
         )
 
-        json_dict = self._get_request(request_url, 1)
+    def test_ends_with(self):
+        with self.context:
+            search_string = users.UserContainer.parse_search_query_params(
+                users.request
+            )
+            self.assertEqual('%%%s' % self.contains_string, search_string)
 
-        self.assertTrue(json_dict[self.top_level_json_key])
 
-    def test_ends_with_user_not_found(self):
-        ends_with_string = 'a'
+class TestParseSearchQueryParamsNone(TestParseSearchQueryParams):
+    def setUp(self):
+        TestParseSearchQueryParams.setUp(self)
+        self.context = app.test_request_context(self.url)
 
-        self.assertNotIn(ends_with_string, self.username)
-
-        request_url = '%s?ends_with=%s' % (
-            self.template_url, ends_with_string
-        )
-
-        json_dict = self._get_request(request_url, 0)
-
-        self.assertFalse(json_dict[self.top_level_json_key])
-
-    def test_empty_search_string(self):
-        request_url = self.template_url
-
-        self._get_request(request_url, 1)
-
-    def _get_request(self, request_url, expected_count):
-        r = self.request_method(request_url, headers=self.headers)
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(int(r.headers['Count']), expected_count)
-
-        json_dict = json.loads(r.data.decode(self.api_charset))
-
-        return json_dict
+    def test_none(self):
+        with self.context:
+            self.assertEqual(
+                '%%%%', users.UserContainer.parse_search_query_params(
+                            users.request
+                    )
+            )
 
 
 @mock.patch('sqlalchemy.orm.Session.add')
@@ -211,7 +174,8 @@ class TestGet(TestUserView):
         self.request_method = self.client.get
         self.url = 'api/v1/users/%s' % self.username
         self.headers['Authorization'] = 'Basic %s' % \
-            b64encode(('%s:%s' % (self.username, self.password)).encode('ascii')).decode('ascii')
+            b64encode(('%s:%s' % (self.username, self.password)).
+                      encode('ascii')).decode('ascii')
 
         self.bad_user = User('foo', 'bar', 'foo@bar.com')
 
