22fa4a548db12917ae8d17b14ee6e6063a52f524
Michal Kononenko <michalkononenko@gmail.com>
2016-01-28 11:11:57 -0500

    Removed JSON Schema file

--- json_schema.py
+++
@@ -1,8 +0,0 @@
-"""
-Contains an abstract representation of a REST API view, as well as a
-subclass of Marshmallow
-"""
-from marshmallow import fields, Schema, ValidationError
-import re
-
-__author__ = 'Michal Kononenko'
