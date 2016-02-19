3fedf49ee9444605501269df07569de173ce73d7
telleris <alex.lee42394@gmail.com>
2016-02-19 11:36:33 -0500

    Basic docopt example

--- tests/unit/test_command_line_interface/test_example.py
+++ tests/unit/test_command_line_interface/test_example.py
@@ -4,31 +4,36 @@ Testing the basic CLI example which reprints the input
 
 import unittest
 import subprocess
+import os
+from config import default_config as config
+
 
 class TestExample(unittest.TestCase):
     def setUp(self):
-        self.command_to_run = 'python ../../../command_line_interface/example.py'
+        self.command_to_run = 'python %s/command_line_interface/example.py' % config.BASE_DIRECTORY
 
     def test_example(self):
-        completed_command = subprocess.run(self.command_to_run,stderr=subprocess.PIPE)
+        completed_command = subprocess.run(self.command_to_run, stderr=subprocess.PIPE)
         self.assertEqual(completed_command.returncode, 1)
-        self.assertEqual(completed_command.stderr.decode("utf-8").rstrip(), 'Usage: example.py [ -h | --help ] (TEXT) ...')
+        self.assertEqual(completed_command.stderr.decode("utf-8").rstrip(),
+                         'Usage: example.py [ -h | --help ] (TEXT) ...')
+
 
 class TestString(unittest.TestCase):
     def setUp(self):
-        self.command_to_run = 'python ../../../command_line_interface/example.py HELLO WORLD'
+        self.command_to_run = 'python %s/command_line_interface/example.py HELLO WORLD' % config.BASE_DIRECTORY
 
     def test_string(self):
-        completed_command = subprocess.run(self.command_to_run,stdout=subprocess.PIPE)
-        self.assertEqual(completed_command.returncode,0)
-        self.assertEqual(completed_command.stdout.decode("utf-8").rstrip(),'HELLO WORLD')
+        completed_command = subprocess.run(self.command_to_run, stdout=subprocess.PIPE)
+        self.assertEqual(completed_command.returncode, 0)
+        self.assertEqual(completed_command.stdout.decode("utf-8").rstrip(), 'HELLO WORLD')
+
 
 class TestHelp(unittest.TestCase):
     def setUp(self):
-        self.command_to_run = 'python ../../../command_line_interface/example.py -h'
+        self.command_to_run = 'python %s/command_line_interface/example.py -h' % config.BASE_DIRECTORY
 
     def test_help(self):
-        completed_command = subprocess.run(self.command_to_run,stdout=subprocess.PIPE)
-        print(completed_command.stdout)
-        self.assertEqual(completed_command.returncode,0)
+        completed_command = subprocess.run(self.command_to_run, stdout=subprocess.PIPE)
+        self.assertEqual(completed_command.returncode, 0)
         self.assertIsNotNone(completed_command.stdout)
