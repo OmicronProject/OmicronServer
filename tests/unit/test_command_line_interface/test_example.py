1196b7a10558e0107085ff519d7082d789527034
telleris <alex.lee42394@gmail.com>
2016-02-19 14:16:46 -0500

    Basic docopt example

--- tests/unit/test_command_line_interface/test_example.py
+++ tests/unit/test_command_line_interface/test_example.py
@@ -1,10 +1,8 @@
 """
 Testing the basic CLI example which reprints the input
 """
-
 import unittest
 import subprocess
-import os
 from config import default_config as config
 
 
@@ -18,7 +16,6 @@ class TestExample(unittest.TestCase):
     def test_example(self):
         completed_command = subprocess.Popen(self.command_to_run, stderr=subprocess.PIPE)
         completed_command.wait()
-        print(completed_command.stderr)
         self.assertEqual(completed_command.returncode, 1)
         self.assertEqual(
             completed_command.stderr.read().decode('utf-8').rstrip(),
@@ -26,18 +23,17 @@ class TestExample(unittest.TestCase):
         )
 
 
-
 class TestString(unittest.TestCase):
     def setUp(self):
         self.command_to_run = [
             'python',
-            '%s/command_line_interface/example.py HELLO WORLD' % config.BASE_DIRECTORY
+            '%s/command_line_interface/example.py' % config.BASE_DIRECTORY,
+            'HELLO WORLD'
             ]
 
     def test_string(self):
         completed_command = subprocess.Popen(self.command_to_run, stdout=subprocess.PIPE)
         completed_command.wait()
-        print(completed_command.stdout)
         self.assertEqual(completed_command.returncode, 0)
         self.assertEqual(completed_command.stdout.read().decode('utf-8').rstrip(), 'HELLO WORLD')
 
@@ -46,7 +42,8 @@ class TestHelp(unittest.TestCase):
     def setUp(self):
         self.command_to_run = [
             'python',
-            '%s/command_line_interface/example.py -h' % config.BASE_DIRECTORY
+            '%s/command_line_interface/example.py' % config.BASE_DIRECTORY,
+            '-h'
             ]
 
     def test_help(self):
