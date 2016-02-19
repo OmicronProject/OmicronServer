ea698535da996f120d2fbc2cd1f7803a8cb03697
telleris <alex.lee42394@gmail.com>
2016-02-19 14:00:35 -0500

    Basic docopt example

--- tests/unit/test_command_line_interface/test_example.py
+++ tests/unit/test_command_line_interface/test_example.py
@@ -13,10 +13,15 @@ class TestExample(unittest.TestCase):
         self.command_to_run = 'python %s/command_line_interface/example.py' % config.BASE_DIRECTORY
 
     def test_example(self):
-        completed_command = subprocess.run(self.command_to_run, stderr=subprocess.PIPE)
+        completed_command = subprocess.Popen(self.command_to_run, stderr=subprocess.PIPE)
+        completed_command.wait()
+        print(completed_command.stderr)
         self.assertEqual(completed_command.returncode, 1)
-        self.assertEqual(completed_command.stderr.decode("utf-8").rstrip(),
-                         'Usage: example.py [ -h | --help ] (TEXT) ...')
+        self.assertEqual(
+            completed_command.stderr.read().decode('utf-8').rstrip(),
+            'Usage: example.py [ -h | --help ] (TEXT) ...'
+        )
+
 
 
 class TestString(unittest.TestCase):
@@ -24,9 +29,11 @@ class TestString(unittest.TestCase):
         self.command_to_run = 'python %s/command_line_interface/example.py HELLO WORLD' % config.BASE_DIRECTORY
 
     def test_string(self):
-        completed_command = subprocess.run(self.command_to_run, stdout=subprocess.PIPE)
+        completed_command = subprocess.Popen(self.command_to_run, stdout=subprocess.PIPE)
+        completed_command.wait()
+        print(completed_command.stdout)
         self.assertEqual(completed_command.returncode, 0)
-        self.assertEqual(completed_command.stdout.decode("utf-8").rstrip(), 'HELLO WORLD')
+        self.assertEqual(completed_command.stdout.read().decode('utf-8').rstrip(), 'HELLO WORLD')
 
 
 class TestHelp(unittest.TestCase):
@@ -34,6 +41,7 @@ class TestHelp(unittest.TestCase):
         self.command_to_run = 'python %s/command_line_interface/example.py -h' % config.BASE_DIRECTORY
 
     def test_help(self):
-        completed_command = subprocess.run(self.command_to_run, stdout=subprocess.PIPE)
+        completed_command = subprocess.Popen(self.command_to_run, stdout=subprocess.PIPE)
+        completed_command.wait()
         self.assertEqual(completed_command.returncode, 0)
         self.assertIsNotNone(completed_command.stdout)
