3c08c56e35aff0d4c3051a27bc0270989b4b6524
Michal Kononenko <michalkononenko@gmail.com>
2016-02-23 10:11:25 -0500

    Versioning info moved to setup.py, to avoid confusion in __init__ file

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 from setuptools import setup
-import omicron_server
+
+__author__ = 'Michal Kononenko'
+__license__ = "GPL-3.0"
+__version__ = '0.1'
 
 setup(
-    name='OmicronServer',
-    version=omicron_server.__version__,
-    author=omicron_server.__author__,
+    name='omicron_server',
+    version=__version__,
+    author=__author__,
     author_email='mkononen@uwaterloo.ca',
-    maintainer='MichalKononenko',
+    maintainer='Michal Kononenko',
     maintainer_email='mkononen@uwaterloo.ca',
     description='The back end to the Omicron Project stack.',
-    license=omicron_server.__license__,
+    license=__license__,
     url='https://github.com/MichalKononenko/OmicronServer',
     packages=['omicron_server']
     )
